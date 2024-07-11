200ae85da42ac37315c6dea86ca6377be1b38c3b
David <david.odejimi677@gmail.com>
2024-07-10 23:29:18 -0500

    Add new dependencies and components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,40 +19,39 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
 
   const account = await getAccount({ appwriteItemId })
 
-const Home = () => {
-    return (
-        <section className="home">
-          <div className="home-content">
-            <header className="home-header">
-              <HeaderBox 
-                type="greeting"
-                title="Welcome"
-                user={loggedIn?.firstName || 'Guest'}
-                subtext="Access and manage your account and transactions efficiently."
-              />
-    
-              <TotalBalanceBox 
-                accounts={accountsData}
-                totalBanks={accounts?.totalBanks}
-                totalCurrentBalance={accounts?.totalCurrentBalance}
-              />
-            </header>
-    
-            <RecentTransactions 
-              accounts={accountsData}
-              transactions={account?.transactions}
-              appwriteItemId={appwriteItemId}
-              page={currentPage}
-            />
-          </div>
-    
-          <RightSidebar 
-            user={loggedIn}
-            transactions={account?.transactions}
-            banks={accountsData?.slice(0, 2)}
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
           />
-        </section>
-      )
-    }
-    
-    export default Home
+
+          <TotalBalanceBox 
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
+
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
+    </section>
+  )
+}
+
+export default Home
