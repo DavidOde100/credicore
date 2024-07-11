70968f948c8f1aad9bcf81ef55543d26ae781610
David <david.odejimi677@gmail.com>
2024-07-11 10:56:26 -0500

    Refactor Home component and handle error when user is not logged in

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,12 +5,26 @@ import TotalBalanceBox from '@/components/TotalBalanceBox';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 
+// const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+//   const currentPage = Number(page as string) || 1;
+//   const loggedIn = await getLoggedInUser();
+//   const accounts = await getAccounts({ 
+//     userId: loggedIn.$id
+//   })
+
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const currentPage = Number(page as string) || 1;
-  const loggedIn = await getLoggedInUser();
-  const accounts = await getAccounts({ 
-    userId: loggedIn.$id 
-  })
+  
+  try {
+    const loggedIn = await getLoggedInUser();
+    
+    if (!loggedIn) {
+      throw new Error('User is not logged in.');
+    }
+
+    const accounts = await getAccounts({ 
+      userId: loggedIn.$id
+    });
 
   if(!accounts) return;
   
